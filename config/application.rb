afd1b5f3a9f5e3d7c7fef42dcfd0324c9464ea40
georgiebrown <georginaabrown@gmail.com>
2020-11-19 08:54:53 +1100

    Update

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Defaceme
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    # config.autoloader = :classic
+    config.autoloader = :classic
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
